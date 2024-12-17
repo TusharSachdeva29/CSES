8d77b0d38fd03e2533192915f33692ba31f16a29
Tushar <sachdevatushar81@gmail.com>
2024-12-17 14:40:31 +0530

    rectangle cutting

---
+++ cses/Rectangle-cutting.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define itn long long  
+#define int long long int
+
+#define Tushar ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define vin(a) for (auto &x : (a)) cin >> x
+#define vout(a) for (const auto &x : (a)) cout << x << ' '; cout << endl
+#define vpin(a) for (auto &x : (a)) cin >> x.first >> x.second
+#define vpout(a) for (const auto &x : (a)) cout << x.first << ' ' << x.second << endl
+#define all(x) (x).begin(), (x).end()
+#define isort(v) sort(all(v))
+#define dsort(v) sort(all(v), greater<int>())
+#define pb push_back
+#define ff first
+#define ss second
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define debug(x) cerr << #x << " = " << x << endl
+#define r(x) { cout << x << endl; return; }
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+
+// Debugging templates
+#ifdef ONLINE_JUDGE
+#define dbg(x)
+#else
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl
+#endif
+
+void _print(int x) { cerr << x; }
+void _print(string x) { cerr << x; }
+void _print(char x) { cerr << x; }
+void _print(double x) { cerr << x; }
+void _print(long double x) { cerr << x; }
+void _print(unsigned long long x) { cerr << x; }
+
+template <class T, class V> void _print(pair<T, V> p) { cerr << "{"; _print(p.ff); cerr << ", "; _print(p.ss); cerr << "}"; }
+template <class T> void _print(vector<T> v) { cerr << "[ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "]"; }
+template <class T> void _print(set<T> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+template <class T> void _print(multiset<T> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+template <class T, class V> void _print(map<T, V> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+
+// Common type aliases
+using vi = vector<int>;
+using vc = vector<char>;
+using vii = vector<pair<int, int>>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using vvi = vector<vector<int>>;
+using vvc = vector<vector<char>>;
+
+// Utility macros
+#define loop(a, b, i) for (int i = (a); i < (b); ++i)
+#define read(x) int x; cin >> x
+#define PI 3.14159265358979323846
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+
+// TIPS :-> brute force do exist like number can be 0-9 only rememnr tht qs in which 26+26^2+26^3>1000
+const int MOD = 1000000007;
+
+void precompute()
+{
+}
+int a;itn b;
+vvi dp;
+
+inline void solve(){
+    cin>>a>>b;dp.resize(a+1,vi(b+1,MAX));
+    for(int i=1;i<=a;i++){
+        for(itn j=1;j<=b;j++){
+            if(i==j) dp[i][j] = 0;
+            else {
+                // veritcal cut
+                for(itn v=1;v<=i-1;v++){
+                    dp[i][j] = min(dp[i][j] , dp[v][j] + dp[i-v][j]+1);
+                }
+                // making a horizontal cut
+                for(int h=1;h<=j-1;h++){
+                    dp[i][j] = min(dp[i][j] , dp[i][h]+dp[i][j-h]+1);
+                }
+            }
+        }
+    }
+    cout<<dp[a][b];
+}
+
+int32_t main()
+{
+
+    Tushar;
+    precompute();
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
