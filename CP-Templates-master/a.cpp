d75c9cfe8cbfc791980a4f98cde8c0b984c6fc00
Tushar <sachdevatushar81@gmail.com>
2025-01-05 12:21:38 +0530

    updated

---
+++ CP-Templates-master/a.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int ll;
+#define int long long int
+#define itn long long 
+#define Tushar ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define vin(a) for (auto &x : (a)) cin >> x
+#define vout(a) for (const auto &x : (a)) cout << x << ' '; cout << endl
+#define vpin(a) for (auto &x : (a)) cin >> x.first >> x.second
+#define vpout(a) for (const auto &x : (a)) cout << x.first << ' ' << x.second << endl
+#define all(x) (x).begin(), (x).end()
+#define isort(v) sort(all(v))
+#define dsort(v) sort(all(v), greater<int>())
+#define pb push_back
+#define ff first
+#define ss second
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define debug(x) cerr << #x << " = " << x << endl
+#define r(x) { cout << x << endl; return; }
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int x) { cerr << x; }
+void _print(string x) { cerr << x; }
+void _print(char x) { cerr << x; }
+void _print(double x) { cerr << x; }
+void _print(long double x) { cerr << x; }
+void _print(unsigned long long x) { cerr << x; }
+
+template <class T, class V> void _print(pair<T, V> p) { cerr << "{"; _print(p.ff); cerr << ", "; _print(p.ss); cerr << "}"; }
+template <class T> void _print(vector<T> v) { cerr << "[ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "]"; }
+template <class T> void _print(set<T> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+template <class T> void _print(multiset<T> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+template <class T, class V> void _print(map<T, V> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+
+using vi = vector<int>;
+using vii = vector<pair<int, int>>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using vvi = vector<vector<int>>;
+
+#define loop(a, b, i) for (int i = (a); i < (b); ++i)
+#define read(x) int x; cin >> x
+#define PI 3.14159265358979323846
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+
+const int MOD = 1000000007;
+
+void precompute() {}
+
+void solve() {
+    read(n);vector<int> helper(2*n+5,0);
+    for(itn i=0;i<n;i++){
+        read(l);read(r);
+        if(l==r){
+
+        }
+    }
+}
+
+int32_t main() {
+    Tushar;
+    precompute();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
