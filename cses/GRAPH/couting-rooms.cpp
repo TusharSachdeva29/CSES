aa3ea426b9ed6db3bca243cfdacaba651ecaa790
TusharSachdeva29 <161271923+TusharSachdeva29@users.noreply.github.com>
2025-01-20 09:42:42 +0530

    graph-inti

---
+++ cses/GRAPH/couting-rooms.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define itn long long  
+#define int long long int
+
+#define Tushar ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define vin(a) for (auto &x : (a)) cin >> x
+#define vout(a) for (const auto &x : (a)) cout << x << ' '; cout << endl
+#define vpin(a) for (auto &x : (a)) cin >> x.first >> x.second
+#define vpout(a) for (const auto &x : (a)) cout << x.first << ' ' << x.second << endl
+#define all(x) (x).begin(), (x).end()
+#define isort(v) sort(all(v))
+#define dsort(v) sort(all(v), greater<int>())
+#define pb push_back
+#define ff first
+#define ss second
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#define r(x) { cout << x << endl; return; }
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+
+// Debugging templates
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int x) { cerr << x; }
+void _print(string x) { cerr << x; }
+void _print(char x) { cerr << x; }
+void _print(double x) { cerr << x; }
+void _print(long double x) { cerr << x; }
+void _print(unsigned long long x) { cerr << x; }
+
+template <class T, class V> void _print(pair<T, V> p) { cerr << "{"; _print(p.ff); cerr << ", "; _print(p.ss); cerr << "}"; }
+template <class T> void _print(vector<T> v) { cerr << "[ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "]"; }
+template <class T> void _print(set<T> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+template <class T> void _print(multiset<T> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+template <class T, class V> void _print(map<T, V> v) { cerr << "{ "; for (const auto &x : v) { _print(x); cerr << " "; } cerr << "}"; }
+
+// Common type aliases
+using vi = vector<int>;
+using vc = vector<char>;
+using vii = vector<pair<int, int>>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using vvi = vector<vector<int>>;
+using vvc = vector<vector<char>>;
+
+// Utility macros
+#define loop(a, b, i) for (int i = (a); i < (b); ++i)
+#define read(x) int x; cin >> x
+#define PI 3.14159265358979323846
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+
+// TIPS :-> brute force do exist like number can be 0-9 only rememnr tht qs in which 26+26^2+26^3>1000
+const int MOD = 1000000007;
+
+void precompute()
+{
+}
+vvc arr;
+vvi vis;
+itn n,m;
+void dfs(int i,int j){
+    if(i<0 || j<0 || i>=n || j>=m || vis[i][j] || arr[i][j]=='#') return;
+    vis[i][j] = 1;
+    dfs(i+1,j);
+    dfs(i-1,j);
+    dfs(i,j+1);
+    dfs(i,j-1);
+}
+
+inline void solve(){
+    cin>>n>>m;
+    arr.resize(n,vc(m));vis.resize(n,vi(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }itn cnt = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            debug(vis);
+            if(!vis[i][j] && arr[i][j]=='.'){
+                dfs(i,j);cnt++;
+            }
+        }
+    }
+    cout<<cnt<<endl;
+
+}
+
+int32_t main()
+{
+
+    Tushar;
+    precompute();
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
